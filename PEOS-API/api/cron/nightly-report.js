4ac65d2a483a70ed279417b0644b27a5b5bf6d29
Muhammad Arif <92arifawan@gmail.com>
2026-09-06 18:19:59 +0500

    Fix: remove cron for now

--- PEOS-API/api/cron/nightly-report.js
+++ PEOS-API/api/cron/nightly-report.js
@@ -1,7 +1,3 @@
-export default async function handler(req, res) {
-  return res.status(200).json({ 
-    status: "PEOS Alive!", 
-    time: new Date().toISOString(),
-    message: "Cron is working" 
-  })
+export default function handler(req, res) {
+  res.status(200).json({ ok: true, msg: "PEOS API is LIVE" })
 }
