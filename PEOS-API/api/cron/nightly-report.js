fbf1930e778b458961fbe184fd9169c521ebd430
Muhammad Arif <92arifawan@gmail.com>
2026-09-06 18:35:44 +0500

    Fix: remove broken cron to fix build

--- PEOS-API/api/cron/nightly-report.js
+++ PEOS-API/api/cron/nightly-report.js
@@ -1,7 +1,3 @@
 export default function handler(req, res) {
-  res.status(200).json({ 
-    target: "HIT", 
-    status: 200, 
-    msg: "PEOS API IS LIVE - NO LOOP" 
-  })
+  res.status(200).json({ target: "HIT", status: 200 })
 }
